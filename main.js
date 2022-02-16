44bcd2ee8bae330686e4bd67e5400aef2bd934e5
Alexandre Piel <Alexandre.Piel@zooplus.com>
2022-02-16 19:13:55 +0100

    stuffffff

--- main.js
+++ main.js
@@ -108,6 +108,46 @@ const MFX = [
     'Auto Pan',
 ];
 
+const KEY = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'];
+
+const SCALE = [
+    'Chromatic',
+    'Ionian',
+    'Dorian',
+    'Phrygian',
+    'Lydian',
+    'Mixolidian',
+    'Aeolian',
+    'Locrian',
+    'Harm minor',
+    'Melo minor',
+    'Major Blues',
+    'minor Blues',
+    'Diminished',
+    'Com.Dim',
+    'Major Penta',
+    'minor Penta',
+    'Raga 1',
+    'Raga 2',
+    'Raga 3',
+    'Arabic',
+    'Spanish',
+    'Gypsy',
+    'Egyptian',
+    'Hawaiian',
+    'Pelog',
+    'Japanese',
+    'Ryuku',
+    'Chinese',
+    'Bass Line',
+    'Whole Tone',
+    'minor 3rd',
+    'Major 3rd',
+    '4th Interval',
+    '5th Interval',
+    'Octave',
+];
+
 function parsePattern(rawData) {
     const data = [...rawData];
 
@@ -119,16 +159,25 @@ function parsePattern(rawData) {
         .map((c) => String.fromCharCode(c))
         .join('');
 
-    const swing = data[49] > 50 ? data[49] - 128 : data[49];
-
     const pattern = {
         name,
         tempo: data[46] + data[48] * 256 + (data[39] ? 128 : 0),
-        swing: swing === 48 ? 50 : swing === -48 ? -50 : swing,
+        swing: data[49] > 50 ? data[49] - 128 : data[49], // 48 is displayed 50 and -48 -> -50 but fuck it :p
         length: data[50] + 1,
         beat: BEAT[data[51]],
+        key: KEY[data[52]],
+        keyId: data[52],
+        scale: SCALE[data[53]],
+        scaleId: data[53],
+        chordSet: data[54] + 1,
         level: 127 - data[56],
+        gateArp: data[64] + 1,
         mfx: MFX[data[77]],
+        mfxId: data[77],
+        alternate13_14: !!data[85],
+        alternate15_16: !!data[86],
+        chainTo: data[17269] + (data[17263] && 128),
+        chainRepeat: data[17272]
     };
 
     console.log(pattern);
